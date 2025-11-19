37b5d1b72b084bf4d53f3461c05e063d64b1c60a
AlfredoCC1 <ac161198@gmail.com>
2025-11-18 20:52:40 -0500

    Producto

---
+++ src/main/java/cibertec/model/Producto.java
@@ -0,0 +1,27 @@
+package cibertec.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Version
+    private Integer version; //Control de concurrencia
+}
